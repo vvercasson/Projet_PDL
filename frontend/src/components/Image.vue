67f5369a7044dc76b2a2ef8c5caf1e12ae35fa46
Martin Ithurbide <martin.ithurbide@etu.u-bordeaux.fr>
2022-03-09 15:57:38 +0100

    Creation du frontend filtres image + ajout des algo coté serveur

--- frontend/src/components/Image.vue
+++ frontend/src/components/Image.vue
@@ -25,7 +25,48 @@ api.getImage(props.id)
 </script>
 
 <template>
-  <figure id="gallery"></figure>  
+  <figure id="gallery"></figure>
+  
+  <div id="Filtres">
+
+    <div id = "luminosite">
+      <h3 id="light">Luminosité</h3>
+      <button id="increaseLight">+</button>
+      <button id="reduceLight">-</button>
+    </div>
+
+    <div id = "histogramme">
+      <h3 id="titleHisto">Histogramme</h3>
+      <button id="histButton">appliquer</button>
+    </div>
+    
+    <div id = "couleur">
+      <h3 id="titleColor">Filtre couleur</h3>
+      <label for="inputColor">Couleur : </label> 
+      <input type="color" id="inputColor" name="couleur">
+      &nbsp;
+      <button id="colorButton">appliquer</button>
+    </div>
+
+    <div id = "blur">
+      <h3 id="titleBlur">Filtre flou</h3>
+      <select>
+        <option id="moyen">Moyen</option>
+        <option id="gaussien">Gaussien</option>
+      </select>
+      <br>
+      <label for="blurLevel">Niveau de Flou : </label> 
+      <input type="number" id="blurLevel">
+      &nbsp;
+      <button id="blurButton">appliquer</button>
+    </div>
+
+    <div id="contour">
+      <h3 id="titleContour"> Filtre Contour</h3>
+      <button id="light">Filtre contour</button>
+    </div>
+  </div>
+
 </template>
 
 <style></style>
