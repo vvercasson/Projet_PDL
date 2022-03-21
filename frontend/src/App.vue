6576c94ee94246b60e8c3115171c38214bdda8ce
Martin Ithurbide <martin.ithurbide@etu.u-bordeaux.fr>
2022-03-21 17:48:38 +0100

    Modification design frontend

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,11 +3,12 @@
 
 <template>
   <div id="app">
-    <nav>
+    <nav id='menu'>
+      <input type='checkbox' id='responsive-menu' onclick='updatemenu()'><label></label>
       <ul>
         <li>
           <router-link to="/">Home</router-link>
-        </li>
+          </li>
         <li>
           <router-link to="/gallery">Gallery</router-link>
         </li>
@@ -16,7 +17,6 @@
         </li>
       </ul>
     </nav>
-
     <div>
       <h1>PDL - L3</h1>
       <router-view />
@@ -30,27 +30,100 @@
   font-family: Arial, Helvetica, sans-serif;
   color: #2c3e50;
 }
-ul {
-  list-style-type: none;
-  margin: 0;
-  padding: 0;
-  overflow: hidden;
-  background-color: #333;
+
+#menu {
+	background: #7549CC;
+	color: #FFF;
+	height: 45px;
+	padding-left: 18px;
+	border-radius: 10px;
+	border: 2px solid #D83BFF;
 }
-li {
-  float: left;
+#menu ul, #menu li {
+	margin: 0 auto;
+	padding: 0;
+	list-style: none
 }
-li a {
-  display: block;
-  color: white;
-  text-align: center;
-  padding: 14px 16px;
-  text-decoration: none;
+#menu ul {
+	width: 100%;
 }
-.error {
-  color: red;
-  font-weight: bold;
-  list-style-type: none;
-  margin-top: 1em;
+#menu li {
+	float: left;
+	display: inline;
+	position: relative;
+}
+#menu a {
+	display: block;
+	line-height: 41px;
+	padding: 0 14px;
+	text-decoration: none;
+	color: #FFFFFF;
+	font-size: 16px;
+	text-transform: uppercase;
+}
+#menu a.dropdown-arrow:after {
+	content: "\25BE";
+	margin-left: 5px;
+}
+#menu li a:hover {
+	color: #CC49A9;
+	background: #F2F2F2;
+}
+#menu input {
+	display: none;
+	margin: 0;
+	padding: 0;
+	height: 45px;
+	width: 100%;
+	opacity: 0;
+	cursor: pointer
+}
+#menu label {
+	display: none;
+	line-height: 41px;
+	text-align: center;
+	position: absolute;
+	left: 35px
+}
+#menu label:before {
+	font-size: 1.6em;
+	content: "\2261"; 
+	margin-left: 20px;
+}
+#menu ul.sub-menus{
+	height: auto;
+	overflow: hidden;
+	width: 170px;
+	background: #444444;
+	position: absolute;
+	z-index: 99;
+	display: none;
+}
+#menu ul.sub-menus li {
+	display: block;
+	width: 100%;
+}
+#menu ul.sub-menus a {
+	color: #FFFFFF;
+	font-size: 16px;
+}
+#menu li:hover ul.sub-menus {
+	display: block
+}
+#menu ul.sub-menus a:hover{
+	background: #F2F2F2;
+	color: #444444;
+}
+@media screen and (max-width: 800px){
+	#menu {position:relative}
+	#menu ul {background:#111;position:absolute;top:100%;right:0;left:0;z-index:3;height:auto;display:none}
+	#menu ul.sub-menus {width:100%;position:static;}
+	#menu ul.sub-menus a {padding-left:30px;}
+	#menu li {display:block;float:none;width:auto;}
+	#menu input, #menu label {position:absolute;top:0;left:0;display:block}
+	#menu input {z-index:4}
+	#menu input:checked + label {color:white}
+	#menu input:checked + label:before {content:"\00d7"}
+	#menu input:checked ~ ul {display:block}
 }
 </style>
