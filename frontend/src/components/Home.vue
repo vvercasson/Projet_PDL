9259ffaf4f32d6a154aa6dfca3a40ecfcdf4ce1d
Martin Ithurbide <martin.ithurbide@etu.u-bordeaux.fr>
2022-03-22 19:46:54 +0100

    Ajout des fonctionnalités de filtre + enregistrement des images filtrés

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -19,31 +19,7 @@ function getImageList() {
 }
 
 function showImage(event)  {
-  console.log(event.target);
-  var imageid = event.target.value;
-  //router.push({ name: 'selectorImage', params: { id: imageid } })
-  const imageShow = document.getElementById("image");
-  if (imageShow !== null) {
-    var varP = document.getElementById("divImage");
-    if(varP != null){
-      varP.parentNode?.removeChild(varP);
-    }
-
-    const imgElt = document.createElement("img");
-    var imageUrl = "http://localhost:4000/images/"+imageid;
-
-    imgElt.setAttribute("src",imageUrl); 
-    imgElt.setAttribute("id","divImage");
-    //var text = document.createTextNode("l'image");
-    //imgElt.appendChild(text);
-    imageShow.appendChild(imgElt);
-
-    // show filter div
-    var divHide = document.getElementById("Filtres")?.style.visibility;
-    if (divHide == 'hidden') {
-       document.getElementById("Filtres").style.visibility = 'visible';
-    }
-  }
+  router.push({ name: "selectorImage",params: {id: event.target.value}});
 }
 
 
@@ -58,53 +34,7 @@ function showImage(event)  {
       </select>
     </div>
   </div>
-
-  <br>
-  <div id="image"></div>
-
-  <div style="visibility: hidden;" id="Filtres">
-    <div id = "luminosite">
-      <h3 id="light">Luminosité</h3>
-      <button id="increaseLight">-</button>
-      <button id="reduceLight">+</button>
-    </div>
-
-    <div id = "histogramme">
-      <h3 id="titleHisto">Histogramme</h3>
-      <button id="histButton">appliquer</button>
-    </div>
-    
-    <div id = "couleur">
-      <h3 id="titleColor">Filtre couleur</h3>
-      <label for="inputColor">Couleur : </label> 
-      <input type="color" id="inputColor" name="couleur">
-      &nbsp;
-      <button id="colorButton">appliquer</button>
-    </div>
-
-    <div id = "blur">
-      <h3 id="titleBlur">Filtre flou</h3>
-      <select>
-        <option id="moyen">Moyen</option>
-        <option id="gaussien">Gaussien</option>
-      </select>
-      <br>
-      <label for="blurLevel">Niveau de Flou : </label> 
-      <input type="number" id="blurLevel">
-      &nbsp;
-      <button id="blurButton">appliquer</button>
-    </div>
-
-    <div id="contour">
-      <h3 id="titleContour"> Filtre Contour</h3>
-      <button id="light">Filtre contour</button>
-    </div>
-  </div>
 </template>
 
 <style>
-  #divImage{
-    width: 300px;
-    height: 300px;
-  }
 </style>
--- frontend/src/http-api.ts
+++ frontend/src/http-api.ts
@@ -18,6 +18,9 @@ const requests = {
 export const api = {
   getImageList: (): Promise<ImageType[]> => requests.get('images', {}),
   getImage: (id: number): Promise<Blob> => requests.get(`images/${id}`, { responseType: "blob" }),
+  getImageFilterAllParameters: (id: number, algo: string, first: string, second: string): Promise<Blob> => requests.get(`images/${id}?algorithm=${algo}&first=${first}&second=${second}`,{responseType: "blob"}),
+  getImageFilterOneParameters: (id: number, algo: string, first: string): Promise<Blob> => requests.get(`images/${id}?algorithm=${algo}&first=${first}`,{responseType: "blob"}),
+  getImageFilterOnlyAlgo: (id: number, algo: string): Promise<Blob> => requests.get(`images/${id}?algorithm=${algo}`,{responseType: "blob"}),  
   createImage: (form: FormData): Promise<ImageType> => requests.post('images', form),
   deleteImage: (id: number): Promise<void> => requests.delete(`images/${id}`),
 };
